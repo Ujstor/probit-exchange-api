2291801a6f94f2e82c3f685843e3cf4fe172a1f9
ujstor <astipan@gmail.com>
2023-04-08 23:31:40 +0200

    visual and html update

--- website/views.py
+++ website/views.py
@@ -3,8 +3,12 @@ from .api_requests import orders_check, balance_check
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET'])
+@views.route('/')
 def home():
-    data1 = orders_check()
-    data2 = balance_check()
-    return render_template("home.html", data1=data1, data2=data2)
+    data1 = balance_check()
+    return render_template("home.html", data1=data1)
+
+@views.route('/orders')
+def orders():
+    data2 = orders_check()
+    return render_template("orders.html", data2=data2)
